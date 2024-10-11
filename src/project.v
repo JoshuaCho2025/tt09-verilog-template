ea1e4e0f96171cfd1cafbfdcc547caa501d2618a
JoshuaCho2025 <145054637+JoshuaCho2025@users.noreply.github.com>
2024-10-11 14:38:13 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_koggestone_adder4 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,43 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+    wire [3:0] a,b;
+    wire [3:0] sum;
+    wire carry_out;
+
+    assign a = ui_in[3:0];
+    assign b = ui_in[7:4];
+
+    wire [3:0] p;
+    wire [3:0] q;
+    wire [3:0] c;
+
+    assign p = a ^ b;
+    assign q = a & b;
+
+    wire g1_1, g1_2, g1_3;
+    assign g1_1 = g[1] | (p[1] & g[0]);
+    assign g1_2 = g[2] | (p[2] & g[1]);
+    assign g1_3 = g[3] | (p[3] & g[2]);
+
+    wire g2_2, g2_3;
+    assign g2_2 = g1_2 | (p[2] & g[0]);
+    assign g2_3 = g1_3 | (p[3] & g1_1);
+
+    assign c[0] = 0;
+    assign c[1] = g[0];
+    assign c[2] = g1_1;
+    assign c[3] = g2_2;
+    assign carry_out = g2_3;
+
+    assign sum = p ^ c;
+
+    assign uo_out[3:0] = sum;
+    assign uo_out[4] = carry_out;
+    assign uo_out[7:5] = 3'b000;
+    assign uio_out = 8'b00000000;
+    assign uio_oe = 8'b00000000;
+endmodule
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
-endmodule
