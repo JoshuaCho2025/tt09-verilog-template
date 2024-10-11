98225647fee6927700f44f40c8ef4aad09dae1d4
JoshuaCho2025 <145054637+JoshuaCho2025@users.noreply.github.com>
2024-10-11 16:15:27 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -20,13 +20,23 @@ async def test_project(dut):
     dut.a.value = 13
     dut.b.value = 10
 
+    a_vals = [i for i in range(16)]
+    b_vals = [i for i in range(16)]
+    
+    for i in range(len(a_vals))
+        for i in range(len(b_vals))
+            dut.a_value = a_vals[i]
+            dut.b_value = b_vals[i]
+
+            dut._log.info(f"value of outputs are: {dut.sum.value} and {dut.carry_out.value}.")
+            assert dut.sum.value == ((int(dut.a_value) + int(dut.b_value)) % 16) and dut.carry_out.value == (1 if (dut.a_value + dut.b_value >= 16) else 0)
+            await ClockCycles(dut.clk, 10)
+            
+        
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 10)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    dut._log.info(f"value of outputs are: {dut.sum.value} and {dut.carry_out.value}.")
-    assert dut.sum.value == 7 and dut.carry_out.value == 1 
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
